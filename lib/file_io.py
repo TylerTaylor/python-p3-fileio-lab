0f546516585a180c53bd33a3b50872d188e9e799
TylerTaylor <vikingtyty@gmail.com>
2022-12-28 11:16:15 -0600

    Done and passing

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as write_file:
+        write_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as append_file:
+        append_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt') as read_file:
+        return read_file.read()
